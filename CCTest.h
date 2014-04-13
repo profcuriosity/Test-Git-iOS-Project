c5cf012cfb7549fd82e81aff2ebfa6f5a44455e9
profcuriosity <profcuriosity@profcuriositys-MacBook-Pro.local>
2014-04-13 15:48:15 +0800

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by profcuriosity on 4/13/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by profcuriosity on 4/13/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
